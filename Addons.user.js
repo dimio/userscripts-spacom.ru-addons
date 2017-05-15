8747f55a09a92b5cfcbe1ec230761b1d23d08dbc
dimio <dimio@dimio.org>
2017-05-15 14:17:55 +0300

    Added makeElementClickable funct.

--- Addons.user.js
+++ Addons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spacom.ru::Addons
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Include Spacom.ru::Addons library functions on spacom.ru
 // @author       dimio
 // @license      MIT
@@ -16,8 +16,25 @@
 // https://spacom.ru/forum/discussion/47/polzovatelskie-skripty)
 
 (function( window, undefined ) {
+    'use strict';
     window.unsafeWindow = window.unsafeWindow || window;
     var w = unsafeWindow;
+
+    w.waitFor = function( obj, prop, callback ) {
+        var token = setInterval( function () {
+            if ( obj[prop] !== undefined ) {
+                clearInterval( token );
+                callback( obj[prop] );
+            }
+        }, 0 );
+    };
+
+    w.makeElementClickable = function ( elem, icon, title, callback ){
+        let text = elem.innerText;
+        if ( icon ){ text = '<i class="fa ' +icon+ '"></i> ' +text; }
+        elem.innerHTML = '<a href="#" title="' +title+ '" onclick="' +callback+ '; return false;">' +text+ '</a>';
+        return elem;
+    };
 
     w.createMapButton = function( css, id, title ) {
         var last = $("#radar + div");
@@ -33,7 +50,23 @@
         return next;
     };
 
-} )(window);
+    w.createNaviBarButton = function( name, callback ) {
+        var last = $("#navi > div:nth-child(3)");
+        var next = $('<div class="navi_menu_item" onclick="' +callback+ '; return false;">\
+<a href="#">' +name+ '</a></div>').css( {
+            "z-index": last.css("z-index"),
+            "position": last.css("position"),
+            "cursor": last.css("cursor"),
+            "color": last.css("color"),
+            "right": last.css("right"),
+            "bottom": last.css("bottom"),
+            //"bottom": (parseInt(last.css("bottom")) + 40) + "px"
+        } );
+        last.after( next );
+        return next;
+    };
+
+} )( window );
 
 /*
 var head = document.getElementsByTagName('head')[0];
