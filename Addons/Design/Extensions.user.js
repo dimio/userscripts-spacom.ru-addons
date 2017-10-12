8890a5f09d9f12dab3a2133a37c5dc92083d17d3
dimio <dimio@dimio.org>
2017-10-12 12:58:25 +0300

    added a cannon hp

--- Addons/Design/Extensions.user.js
+++ Addons/Design/Extensions.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Spacom::Addons::Design::Extensions
-// @version      0.0.2
+// @version      0.0.3
 // @namespace    http://dimio.org/
 // @description  Extends the functions of a ships constructor
 // @author       dimio (dimio@dimio.org)
@@ -29,11 +29,32 @@ console.log( "Spacom::Addons::Design::Extensions" );
     }
 
     function calcLaserPowerSumm(params){
-       return params.lazer_power * params.lazer_shots;
+        return params.lazer_power * params.lazer_shots;
     }
 
     function calcLaserEqHp(params){
-       return Math.round( params.hp / (1 - params.lazer_defence/100) );
+        return Math.round( params.hp / (1 - params.lazer_defence/100) );
+    }
+
+    function calcLaserHp(hp, laser_eq_hp){
+        return laser_eq_hp - hp;
+    }
+
+    function calcCannonHp(params){
+        return params.hp + params.cannon_defence;
+    }
+
+    function makeDesignInfoTemplate() {
+        var designInfoTemplate = document.getElementById("design_info_template").innerHTML;
+
+        designInfoTemplate = designInfoTemplate.replace("lazer_shots'] + '&nbsp;' : '' %>",
+                                                        "lazer_shots'] + ' <span class=\"lazer_attack\">&sum;&nbsp;</span>' + params['laser_power_summ'] + '&nbsp;' : '' %>");
+        designInfoTemplate = designInfoTemplate.replace("lazer_defence'] + '%&nbsp;' : '' %>",
+                                                        "lazer_defence'] + '%&nbsp;(' + params['laser_hp'] + ' hp)&nbsp;<i class=\"fa fa-heart lazer_attack\" title=\"Приведённые очки прочности с учётом защиты от лазеров\"></i>&nbsp;' + params['laser_eq_hp'] + '&nbsp;' : '' %>");
+        designInfoTemplate = designInfoTemplate.replace("cannon_defence'] + '&nbsp;' : '' %>",
+                                                        "cannon_defence'] + '&nbsp;<i class=\"fa fa-heart cannon_attack\" title=\"Очки прочности с учётом защиты от пушек\"></i>&nbsp;' + params['cannon_hp'] + '&nbsp;' : '' %>");
+
+        document.getElementById("design_info_template").innerHTML = designInfoTemplate;
     }
 
     if (w.Design) {
@@ -43,6 +64,11 @@ console.log( "Spacom::Addons::Design::Extensions" );
                 _designCalc.apply(this, arguments);
                 design.params.laser_power_summ = calcLaserPowerSumm(design.params);
                 design.params.laser_eq_hp = calcLaserEqHp(design.params);
+                design.params.laser_hp = calcLaserHp(
+                    design.params.hp,
+                    design.params.laser_eq_hp
+                );
+                design.params.cannon_hp = calcCannonHp(design.params);
             };
 
             var _designDraw = design.draw;
@@ -51,12 +77,7 @@ console.log( "Spacom::Addons::Design::Extensions" );
                 $("#design_info").html(tmpl(document.getElementById("design_info_template").innerHTML, this));
             };
 
-            var designInfoTemplate = document.getElementById("design_info_template").innerHTML;
-            designInfoTemplate = designInfoTemplate.replace("lazer_shots'] + '&nbsp;' : '' %>",
-                                                            "lazer_shots'] + ' <span class=\"lazer_attack\">&sum;&nbsp;</span>' + params['laser_power_summ'] + '&nbsp;' : '' %>");
-            designInfoTemplate = designInfoTemplate.replace("lazer_defence'] + '%&nbsp;' : '' %>",
-                                      "lazer_defence'] + '%&nbsp;<i class=\"fa fa-heart lazer_attack\" title=\"Приведённые очки прочности с учётом защиты от лазеров\"></i>&nbsp;' + params['laser_eq_hp'] + '&nbsp;' : '' %>");
-            document.getElementById("design_info_template").innerHTML = designInfoTemplate;
+            makeDesignInfoTemplate();
 
             design.calc();
             design.draw();
